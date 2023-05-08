4d8dc072a1c7911179243f51d670b079af81767a
Matheus Doerzbacher <Matheus.doer@gmail.com>
2023-05-08 14:59:35 -0300

    Final aula 197

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Layout from '@/components/Layout'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline ">
-      Hello world!
+    <h1 className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo='Cadastro Simples'>
+        <span>Conteudo</span>
+      </Layout>
     </h1>
   )
 }
