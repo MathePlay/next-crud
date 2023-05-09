2395e142cb0d339edfc5a548004368ba2a028dce
MathePLay <Matheus.doer@gmail.com>
2023-05-09 10:22:30 -0300

    FInal aula 201

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,26 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/Layout'
+import Tabela from '@/components/Tabela'
+import Cliente from '@/core/CLiente'
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '5'),
+  ]
+
+  function clienteSelecionado() {
+
+  }
+
+  function clienteExcluido() {
+
+  }
+
   return (
     <h1 className={`
       flex justify-center items-center h-screen
@@ -12,7 +30,11 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo='Cadastro Simples'>
-        <span>Conteudo</span>
+        <Tabela 
+          clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
       </Layout>
     </h1>
   )
